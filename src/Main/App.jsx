103eb7b9b2b23ec2ca0dea2602ca9c4ffca50eb5
Nathan Ferreira <nathanferreira00@hotmail.com>
2020-12-30 15:25:57 -0300

    Grid deifinida

---
+++ src/Main/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+
+import Nav from '../components/templates/Nav'
+import Main from '../components/templates/Main'
+import Footer from '../components/templates/Footer'
+
+export default props =>
+    <div className="app">
+        <Nav/>
+        <Main/>
+        <Footer/>
+    </div>
